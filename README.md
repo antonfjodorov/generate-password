5b5a21d0c45b112aa7db30b5550718543d50f689
Anton Fjodorov <antonfjodorov@gmail.com>
2017-03-23 00:01:28 +0100

    1. new feature: auto-copy to clipboard, dependent on pyperclip. updated README accordingly 2. colorful output for better UX

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # generate-password
 Terminal "plugin" to quickly generate cryptographically strong passwords.
 
+## Dependencies
+- pyperclip. Install it via `pip install pyperclip`
+
 ## Installation
 ### Linux
 - Clone repo into whatever_folder
@@ -12,27 +15,16 @@ Terminal "plugin" to quickly generate cryptographically strong passwords.
 Contact me to add installation instructions here
 
 ## Code
-Char space: uppercase ASCII, lowercase ASCII, digits and %`\'()*+-,./:;<>=!_&~{}|^?$#@"
-Random number generator: [os.urandom(1)](https://docs.python.org/3.4/library/os.html#os.urandom).
+- Char space: uppercase ASCII, lowercase ASCII, digits and %`\'()*+-,./:;<>=!_&~{}|^?$#@"
+- Random number generator: [os.urandom(1)](https://docs.python.org/3.4/library/os.html#os.urandom).
 
 ## Examples
 | # | Command | Output | Description |
 | - | - | - | - |
-| 1 |`generatePassword`| `U($mfeLNwv` | 10 random characters (10 is default)|
-| 2 |`generatePassword 20`| `:doAV@YzS18pos:e@.no` |20 random chars|
-| 3 |`generatePassword omit uppercase`|`qtpfx-!f5*`|10 chars, no uppercase chars|
-| 4 |`generatePassword omit asdfghjk1`|`}Oe~Gix={T`|10 chars, excluding specified list of chars|
+| 1 | `generatePassword`| `U($mfeLNwv` | 10 random characters (10 is default) |
+| 2 | `generatePassword 20` | `:doAV@YzS18pos:e@.no` | 20 random chars |
+| 3 | `generatePassword --omit uppercase` | `qtpfx-!f5*` | 10 chars, no uppercase chars |
+| 4 | `generatePassword -o asdfghjk1` | `}Oe~Gix={T` | 10 chars, excluding specified list of chars |
+| 4 | `generatePassword -o digits -c 12` | `R/ts!IkMgj'g` | 12 chars, no digits, copied to clipboard |
 
 Other character groups you can omit (like in #3) are _uppercase_, _lowercase_, _digits_ and _special_.
-
-## Todo
-- Copy to clipboard function with import Tkinter
-
-```
-	from Tkinter import Tk
-	r = Tk()
-	r.withdraw()
-	r.clipboard_clear()
-	r.clipboard_append('i can has clipboardz?')
-	r.destroy()
-```
--- generatePassword.py
+++ generatePassword.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python
 #-*- coding: utf-8 -*-
+from colorconsole import ColorConsole as Console
+import getopt
 import os
+import pyperclip
 import string
 import sys
 
@@ -29,37 +32,67 @@ class Program(object):
 		msg = ''
 		msg += 'Generate random password.'
 		msg += '\n'
-		msg += '\nUsage:\t     ./genPw [options] [length]'
-		msg += '\n or:\tpython genPw [options] [length]'
+		msg += '\nUsage:       ./genPw [options] [length]'
+		msg += '\n or:    python genPw [options] [length]'
 		msg += '\n'
-		msg += '\nOptions:'
-		msg += '\n  -o <C>, omit <C>\tomit one or several characters (C) from the pool of possible characters'
-		msg += '\n  -o <G>, omit <G>\tomit a group of characters (G) from the pool of possible characters,'
-		msg += '\n                    \twhere G is either of {lowercase, uppercase, digits, special}'
+		msg += '\nOptions (can be combined):'
+		msg += '\n  -o <C>, --omit <C>  omit one or several characters (C) from the pool of possible'
+		msg += '\n                      characters.'
+		msg += '\n  -o <G>, --omit <G>  omit a group of characters (G) from the pool of possible,'
+		msg += '\n                      characters where G is either of {lowercase, uppercase,'
+		msg += '\n                      digits, special}'
+		msg += '\n  -c, --copy          copy password to clipboard'
+		msg += '\n'
 		msg += '\nLength:'
-		msg += '\n  [length]\t\toptionally generate a password of another length than 10'
+		msg += '\n  [length]            optionally generate a password of another length than 10'
 		msg += '\n'
 		msg += '\nExamples:'
-		msg += '\n  ./genPw\t\t\t43zJvQJX_a\t\ta password of length 10'
-		msg += '\n  ./genPw 20\t\t\tP&FsOetjNyP=IHc2wpUf\ta password of length 20'
-		msg += '\n  ./genPw -o ilO 15\t\tf!WKGLh5U6r,QRs\t\ta password of length 15, without i, l and O'
-		msg += '\n  ./genPw omit uppercase\t.~f*j0!*17\t\ta password of length 10, without uppercase characters'
+		msg += '\n  ./genPw                         '+Console.orange('43zJvQJX_a')+'      a password of length 10'
+		msg += '\n  ./genPw -c 15                   '+Console.orange('P&FsOetjNyP=IHc')+' a password of length 15, copied'
+		msg += '\n                                                  to clipboard. Press Ctrl-V to'
+		msg += '\n                                                  paste it.'
+		msg += '\n  ./genPw -o ilO 15               '+Console.orange('f!WKGLh5U6r,QRs')+' a password of length 15, no'
+		msg += '\n                                                  letters i, l and O'
+		msg += '\n  ./genPw --omit uppercase        '+Console.orange('.~f*j0!*17')+'      a password of length 10, no'
+		msg += '\n                                                  uppercase chars'
+		msg += '\n  ./genPw -o digits -o special -c '+Console.orange('XimoMKZDtT')+'      a password of length 10, no'
+		msg += '\n                                                  digits or special chars'
 		return msg
 
-if __name__ == "__main__":
-	p = Program()
-	if len(sys.argv) < 2:
-		p.genPw()
-	else:
-		if 'help' in sys.argv[1]:
-			print p.usage()
-			sys.exit(0)
-		elif 'omit' in sys.argv[1] or '-o' in sys.argv[1]:
-			p.omitChars(sys.argv[2])
-			if len(sys.argv) == 3:
-				p.genPw()
-			elif len(sys.argv) == 4:
-				p.genPw(int(sys.argv[3]))
-		else:
-			p.genPw(int(sys.argv[1]))
+if __name__ != "__main__":
+	sys.exit(0);
+
+doCopy = False
+argNum = len(sys.argv)
+p = Program()
+if argNum < 2:
+	p.genPw()
 	print p.getPw()
+	sys.exit(0)
+
+try:
+	opts, args = getopt.getopt(sys.argv[1:], "cho:", ["clipboard", "help", "omit="])
+except getopt.GetoptError as e:
+	p.usage()
+	sys.exit(1)
+for opt, arg in opts:
+	if opt == '-c' or opt == '--clipboard':
+		doCopy = True
+	elif opt == '-h' or opt == '--help':
+		print p.usage()
+		sys.exit(0)
+	elif opt == '-o' or opt == '--omit':
+		p.omitChars(arg)
+if not args:
+	p.genPw()
+else:
+	try:
+		int_arg = int(args[0])
+		p.genPw(int_arg)
+	except:
+		print Console.red('Error'), 'password length must be a number. Your input:', Console.orange(args[0])
+		sys.exit(1)
+print p.getPw()
+if doCopy:
+	pyperclip.copy(p.getPw())
+	print Console.orange('copied to clipboard')
